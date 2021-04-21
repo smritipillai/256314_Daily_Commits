09adb5bb7ba6f47a4a8122ea6d28fb86e236edda
Smriti <smritipillai.smriti@gmail.com>
2021-04-21 12:42:26 +0530

    list

---
+++ lists.py
@@ -0,0 +1,14 @@
+words = ["Hello", "world", "!"]  
+
+print(words[0])
+print(words[1])
+print(words[2])
+
+empty = []
+print(empty)
+
+num = 3
+t = ["string", 0, [1,2,num], 4.56]
+print(t[1])
+print(t[2])
+print(t[2][2])
