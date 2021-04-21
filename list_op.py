ab7083fd96f090dee090cf7bdab729d4105542c9
Smriti <smritipillai.smriti@gmail.com>
2021-04-21 13:14:40 +0530

    list_op

---
+++ list_op.py
@@ -0,0 +1,22 @@
+nums = [1, 2, 3, 4, 5]
+nums[3] = nums[1]
+print(nums[3])
+
+num = [1,2,3]
+num.append(4)
+print(num)
+
+words = ["hello"]
+words.append("world")
+print(words[1])
+
+print(len(words))
+
+
+letters = ['a', 'b', 'c', 'd', 'e']
+print(letters.index('d'))
+print(max(letters))
+print(letters.count('a'))
+letters.remove('d')
+letters.reverse()
+print(letters)
